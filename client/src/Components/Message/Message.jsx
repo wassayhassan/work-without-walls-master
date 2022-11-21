ee825c3a9fd499e133a62c43cb9c1368d4089f79
wassayhassan <102725500+wassayhassan@users.noreply.github.com>
2022-11-21 23:06:58 +0500

    lN

--- client/src/Components/Message/Message.jsx
+++ client/src/Components/Message/Message.jsx
@@ -4,6 +4,7 @@ import { createOrder, updateBid } from "../../api";
 import { useNavigate } from "react-router-dom";
 import React, {useState, useContext} from "react";
 import { UserContext } from "../../context/user.context";
+import { Card } from "flowbite-react";
 
 export default function Message({currentChat, member, current, message, own }) {
   const [accepted, setAccepted] = useState(message.accepted === "true"? true: false);
@@ -93,25 +94,37 @@ date = date.addDays(parseInt(message.dealTime));
   <div className="messageBottom">{format(message.createdAt)}</div>
   </div>
 
-        <div className="card p-1" style={{width: "25rem"}}>
-        <div className="card-body">
-          <h4 className="card-title">Seller Offer</h4>
-          <h6>Description</h6>
-          <p className="card-text">{message.offer}</p>
-          {/* <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a> */}
-        </div>
-        <div className="ml-3">
-          <h5>Offer Includes</h5>
-          <li> {message.dealTime} days delivery</li>
-          <li>{message.percentOff} % Off</li>
-          <li>${message.budget}</li>
-        </div>
+        <Card className="h-80 shadow-md rounded-lg p-1" style={{width: "25rem"}}>
+          <div className="flex flex-row justify-between">
+            <div>
+                <div className="">
+                    <h4 className="font-semibold">Seller Offer</h4>
+                    <p className="text-base font-medium">Description</p>
+                    <p className="">{message.offer}</p>
+                    {/* <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a> */}
+                  </div>
+                    <div className="">
+                      <p className="font-medium text-base">Offer Includes</p>
+                      <li> {message.dealTime} days delivery</li>
+                      <li>{message.percentOff} % Off</li>
+
+                    </div>
+            </div>
+
+              <div>
+                <p className="font-semibold text-lg">${message.budget}</p>
+              </div>
+          </div>
+
+
+
+
         <div className="d-flex flex-row justify-content-end">
        {accepted && <button type="button" className="btn btn-dark" onClick={()=> navigate(`/user/manage/order/${offerOrderId}`)}>View Order</button>}
         {(message.senderId === user._id? <button type="button" className="btn btn-light" onClick={handleOfferWithDraw} disabled={Withdraw}>{Withdraw? 'Withdrawn': 'Withdraw Offer'}</button>:<button type="button" className="btn btn-light" onClick={handleAccept} disabled={accepted}>{accepted? 'Accepted': 'Accept'}</button>)}
         </div>
-      </div>
+      </Card>
   
 </div>
 </div>
